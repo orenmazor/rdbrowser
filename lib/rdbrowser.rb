133b1962a64e0919dc738393d060ff2a0fa59fe3
Oren Mazor <oren.mazor@gmail.com>
2014-04-11 15:01:55 -0400

    list keys without ttl

--- lib/rdbrowser.rb
+++ lib/rdbrowser.rb
@@ -1,5 +1,39 @@
-require "rdbrowser/version"
+require "./lib/rdbrowser/version"
+require 'rdb'
 
 module Rdbrowser
-  # Your code goes here...
+  class MyCallbacks
+    include RDB::ReaderCallbacks
+  end
+
+  def self.list_databases(path_to_db)
+    db_list = MyCallbacks.new
+    db_list.instance_eval "def start_database(database); puts database; end"
+
+    RDB::Reader.read_file(path_to_db, callbacks: db_list) 
+  end
+
+  def self.list_keys(path_to_db, filter = ".*")
+    key_lister = MyCallbacks.new
+    key_lister.instance_eval "KEY_SELECTOR = Regexp.compile(/#{filter}/)"
+    key_lister.instance_eval "def accept_key?(state); state.database == 0 && KEY_SELECTOR.match(state.key); end"
+    key_lister.instance_eval "def set(value, key, state); puts key; end"
+
+    RDB::Reader.read_file(path_to_db, callbacks: key_lister)
+  end
+
+  def self.list_keys_with_ttl(path_to_db)
+    key_lister = MyCallbacks.new
+    key_lister.instance_eval "def accept_key?(state); !state.key_expiration.nil?; end"
+    key_lister.instance_eval "def set(key, value, state); puts key; end"
+    RDB::Reader.read_file(path_to_db, callbacks: key_lister)
+  end
+
+  def self.list_keys_without_ttl(path_to_db)
+    key_lister = MyCallbacks.new
+    key_lister.instance_eval "def accept_key?(state); state.send(:key_expiration).nil?; end"
+    key_lister.instance_eval "def set(key, value, state); puts key; end"
+    RDB::Reader.read_file(path_to_db, callbacks: key_lister)
+  end
+
 end
